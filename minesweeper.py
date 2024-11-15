ec99468d0e5a1683bcb0d0004e1dc7991537cc63
Kenji Kitamura <kenji@rainbowdevil.jp>
2024-11-15 22:43:13 +0900

    仮のボードクラスと描画を実装

--- minesweeper.py
+++ minesweeper.py
@@ -8,11 +8,27 @@ class Vec2:
         self.x = x
         self.y = y
 
+
+class Cell:
+    TYPE_EMPTY = 0
+    TYPE_BOMB = 1
+
+    def __init__(self):
+        self.type = Cell.TYPE_EMPTY
+class Board:
+    def __init__(self, width, height):
+        self.grid = [[Cell() for _ in range(width)] for _ in range(height)]
+
+    def draw(self):
+        for y, raw in enumerate(self.grid):
+            for x, item in enumerate(raw):
+                pyxel.text(x*10, y*10, "O", 5)
 class App:
     def __init__(self):
         pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Test", fps=60)
         pyxel.mouse(True)
         self.pos = Vec2(0,0)
+        self.board = Board(10,10)
         pyxel.run(self.update, self.draw)
         
 
@@ -28,7 +44,6 @@ class App:
         pyxel.cls(0)
         pyxel.text(55,41,"Hello, Pyxel!", pyxel.frame_count % 16)
         pyxel.text(self.pos.x, self.pos.y, "HERE!", 5)
-        #pyxel.blt(61, 66, 0,0,0,38,16)
-
+        self.board.draw()
 App()
         
